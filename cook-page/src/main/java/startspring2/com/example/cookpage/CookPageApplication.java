47f326d5c5157ec74a8290d8cb5e0a4aa5ab7232
Joanna Frąckowiak <joanna.maria.frackowiak@gmail.com>
2020-11-30 20:02:55 +0100

    First Commit

---
+++ cook-page/src/main/java/startspring2/com/example/cookpage/CookPageApplication.java
@@ -0,0 +1,13 @@
+package startspring2.com.example.cookpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookPageApplication.class, args);
+	}
+
+}
---
+++ cook-page/src/test/java/startspring2/com/example/cookpage/CookPageApplicationTests.java
@@ -0,0 +1,13 @@
+package startspring2.com.example.cookpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
